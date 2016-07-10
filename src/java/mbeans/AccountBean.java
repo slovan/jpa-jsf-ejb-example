2e4a492a14d6bb595d65b761d67cb7d1bda404c4
Volodymyr Ponomarenko <volod.ponomar@gmail.com>
2016-07-10 15:38:28 +0200

    add response page and some fixes

--- src/java/mbeans/AccountBean.java
+++ src/java/mbeans/AccountBean.java
@@ -7,8 +7,10 @@ package mbeans;
 
 
 import entities.Account;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import sessions.AccountFacade;
 
 /**
  *
@@ -17,11 +19,20 @@ import javax.faces.bean.RequestScoped;
 @ManagedBean
 @RequestScoped
 public class AccountBean {
-
+    
+    @EJB
+    private AccountFacade facade;
+    
     private Account account;
     
     
     public AccountBean() {
+        account = new Account();
+    }
+    
+    public String actionMethod() {
+        facade.openAccount(account.getOwnername(), account.getBalance());
+        return "response";
     }
 
     public Account getAccount() {
@@ -31,6 +42,15 @@ public class AccountBean {
     public void setAccount(Account account) {
         this.account = account;
     }
+
+    public AccountFacade getFacade() {
+        return facade;
+    }
+
+    public void setFacade(AccountFacade facade) {
+        this.facade = facade;
+    }
+    
     
     
 }
